cb9db329db83edacc0eb2393b243c283069a30b5
tahiry-dev <tahirycontact@gmail.com>
2021-01-01 20:31:17 +0300

    create session and allow user to sign-in and sign-out

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+    def new
+    
+    end
+
+    def create
+      user = User.find_by(name: params[:session][:name])
+      if user
+       log_in user
+       redirect_to user
+      
+      else
+       flash.now[:danger] = "Invalid name, Please try again"
+       render :new
+
+      end
+    end
+    
+
+    def destroy
+        log_out
+        redirect_to login_path
+        flash[:notice] = "Thanks for your visit"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:show, :new, :create ]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
